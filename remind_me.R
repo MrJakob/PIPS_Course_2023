a5b657271fa03f7d94e1ef45288a97abfa23d2d3
MrJakob <117581844+MrJakob@users.noreply.github.com>
2023-01-25 09:02:10 +0100

    Add files via upload

---
+++ remind_me.R
@@ -0,0 +1,10 @@
+remind_me <- function() {
+  return(
+    c(
+      "PIPS Exam, 2023-02-03, 14:30 - 17:30",
+      "Statistics 1 Exam, 2023-03-02, 11:00 - 13:00",
+      "Statistics 2 Exam, 2023-03-31, 12:30 - 14:30",
+      "Multilevel Analysis Exam, 2023-06-30, 12:00 - 14:00 "
+    )
+  )
+}
