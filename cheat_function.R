a4b5a92334cc2566a8a28b10d481602b63d8ba7f
MrJakob <117581844+MrJakob@users.noreply.github.com>
2023-01-27 08:16:49 +0100

    Add files via upload

---
+++ cheat_function.R
@@ -0,0 +1,17 @@
+cheat <- function(questionnumber) {
+  if (questionnumber == "3.1.1") {
+    grades <- rnorm(63, 7.2, 1)
+    hist(grades)
+  }
+  else if (questionnumber == "3.1.9") {
+    library(ggstatsplot)
+    library(ggplot2)
+    ggbetweenstats(data = ToothGrowth, x = supp, y = len)
+  }
+  else if (questionnumber == "3.1.17") {
+    print("The shortcut is Ctrl + Shift + A")
+  }
+  else {
+    stop("Input must be a known question number of format 3.1.1")
+  }
+}
